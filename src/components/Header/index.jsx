93c1e257c4da6a8aa9e6a38e40806817ec09cab8
Petr Efimov (Shkoda) <apple-i-shop@yandex.ru>
2022-11-25 19:11:25 +0100

    Desctop version

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,96 @@
+import { useState } from "react";
+
 import classes from "./styles.module.scss";
 
+// Components
+import Logo from "../../assets/svg/logo";
+import CalendarIcon from "../Icons/Calendar";
+import PersonIcon from "../Icons/Person";
+
 const {
-    header
+    header,
+    header__logo,
+    header__nav,
+    header__navMenu,
+    header__navMenuItem,
+    header__navMenuLink,
+    header__navMenuLink_active,
+    header__navButtons,
+    header__navIcon
 } = classes;
 
 export default function Header (props) {
+
+    const menu = [
+        {
+            text: "Home",
+            href: "/"
+        },
+        {
+            text: "Projects",
+            href: "/profile"
+        },
+        {
+            text: "Levels",
+            href: "/levels"
+        },
+        {
+            text: "Swap",
+            href: "/swap"
+        },
+        {
+            text: "FAQ",
+            href: "/faq"
+        },
+        {
+            text: "NFT Marketplace",
+            href: "/nft-marketplace"
+        }
+    ];
+
+    const path = window.location.pathname;
+
     return (
         <header className={header}>
-            Header
+            <Logo className={header__logo} />
+
+            <nav className={header__nav}>
+                <ul className={header__navMenu}>
+                    {
+                        menu.map(({text, href}, i) => {
+
+                            let className = header__navMenuLink;
+
+                            if (href === path || href + "/" === path) {
+                                className += " " + header__navMenuLink_active;
+                            }
+
+                            return (
+                                <li className={header__navMenuItem} key={i}>
+                                    <a href={href} className={className}>
+                                        {text}
+                                    </a>
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+
+                <div className={header__navButtons}>
+                    <a href="#" className={header__navIcon}>
+                        <CalendarIcon />
+                    </a>
+                    <a href="#" className={header__navIcon}>
+                        <PersonIcon />
+                    </a>
+                    <a href="#" className="">
+                        Buy ARTR
+                    </a>
+                    <a href="#" className="">
+                        Connect Wallet                        
+                    </a>
+                </div>
+            </nav>
         </header>
     )
 }
