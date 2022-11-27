2e794a908651eb766a5eeeecf15b2a00f246ea00
Petr Efimov (Shkoda) <apple-i-shop@yandex.ru>
2022-11-27 07:13:05 +0100

    Fx

--- src/components/ProgressbarTimestamp/index.jsx
+++ src/components/ProgressbarTimestamp/index.jsx
@@ -13,10 +13,21 @@ const {
 } = classes;
 
 export default function ProgressbarTimestamp({ value, timestamp, coin, total, current, showPercents, className }) {
+
+   const swapDate = new Date(timestamp);
+
+   const months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+
+   let swapEndString = ("0"+swapDate.getDate()).slice(-2) + " ";
+   swapEndString += months[swapDate.getMonth()] + ", ";
+   swapEndString += swapDate.getFullYear() + " ";
+   swapEndString += ("0"+swapDate.getHours()).slice(-2) + ":";
+   swapEndString += ("0"+swapDate.getMinutes()).slice(-2);
+
    return (
       <div className={progressbar + (className ? " " + className : "")}>
          <div className={progressbar__head}>
-            <p className={progressbar__timestamp}>{"Swap end - " + timestamp}</p>
+            <p className={progressbar__timestamp}>{"Swap end - " + swapEndString}</p>
             {showPercents ? <p className={progressbar__percents}>{value}%</p> : ""}
          </div>
          <div className={progressbar__wrapper}>
