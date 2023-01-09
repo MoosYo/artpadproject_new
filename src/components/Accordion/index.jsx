ecf0ad1b6833da5816cdf9729d53ca199eaf3690
Artyom Anpilogov <47990465+fluffytrolll@users.noreply.github.com>
2023-01-09 13:23:57 +0100

    Implement locale accrodion

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -1,31 +1,29 @@
+import getLocale from "../../helpers/getLoacale";
 import AccordionItem from "../AccordionItem";
 import scss from "./index.module.scss";
+import localize from "./local.json";
 
-const { accordion, accordion__title } = scss;
+const { accordion, accordion__title, accordion__text } = scss;
 
 function Accordion({ className }) {
+   const local = getLocale();
    return (
       <section className={accordion + " " + className}>
          <h1 className={accordion__title}>FAQ</h1>
-         <AccordionItem title={"How can I participate in IDO?"}>
-            <p>
-               Read the full guide in our official documentation 1. Register in your personal account 2. Top up the Main
-               $ARTR wallet with the required number of coins through the Buy ARTR section 3. Go to the Levels page 4.
-               Send the required amount of $ARTR to Staking to get one of the levels. You can see the required amount
-               for staking in the list of levels, as well as the terms of placement and conditions for them. 5. Register
-               for the IDO you want to participate in. The IDO registration period usually starts 72 hours before the
-               start of IDO, you need to open the Projects page and click the “Register” button in the IDO pool card.
-            </p>
+         <AccordionItem title={"1. How can I participate in IDO?"}>
+            <div className={accordion__text} dangerouslySetInnerHTML={{ __html: localize.question_1[local] }}></div>
          </AccordionItem>
-         <AccordionItem title={"How can I participate in IDO?"}>
-            <p>
-               Read the full guide in our official documentation 1. Register in your personal account 2. Top up the Main
-               $ARTR wallet with the required number of coins through the Buy ARTR section 3. Go to the Levels page 4.
-               Send the required amount of $ARTR to Staking to get one of the levels. You can see the required amount
-               for staking in the list of levels, as well as the terms of placement and conditions for them. 5. Register
-               for the IDO you want to participate in. The IDO registration period usually starts 72 hours before the
-               start of IDO, you need to open the Projects page and click the “Register” button in the IDO pool card.
-            </p>
+         <AccordionItem title={"2. What are the steps in IDO?"}>
+            <div className={accordion__text} dangerouslySetInnerHTML={{ __html: localize.question_2[local] }}></div>
+         </AccordionItem>
+         <AccordionItem title={"3. What is First Come First Serve (FCFS)? Who can participate in it?"}>
+            <div className={accordion__text} dangerouslySetInnerHTML={{ __html: localize.question_3[local] }}></div>
+         </AccordionItem>
+         <AccordionItem title={"4. Do I need to spread (unstake) / steak for each IDO?"}>
+            <div className={accordion__text} dangerouslySetInnerHTML={{ __html: localize.question_4[local] }}></div>
+         </AccordionItem>
+         <AccordionItem title={"5. Can I shake (unstake) after each IDO?"}>
+            <div className={accordion__text} dangerouslySetInnerHTML={{ __html: localize.question_5[local] }}></div>
          </AccordionItem>
       </section>
    );
